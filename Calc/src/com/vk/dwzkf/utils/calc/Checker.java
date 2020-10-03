2cc81c9fed8c9a1d52c9c48cc310d4f16fdc1a0d
fkzwd <lives352@gmail.com>
2020-10-03 23:57:04 +0300

    Fixed bug return true for x.y.z numbers

--- Calc/src/com/vk/dwzkf/utils/calc/Checker.java
+++ Calc/src/com/vk/dwzkf/utils/calc/Checker.java
@@ -4,7 +4,7 @@ public class Checker {
     private final char[] mathOperations = {'+','-','/','*','^'};                //1  --  1
     private final char[] hooks = {'(',')'};                                     //2  --  1 << 1
     private final char[] digits = {'0','1','2','3','4','5','6','7','8','9'};    //4  --  1 << 2
-    private final char[] delimiters = {'.'};                                    //8  --  1 << 3
+    private final char[] delimiters = {'.',','};                                    //8  --  1 << 3
     private final char[] minus = {'-'};                                         //16 --  1 << 4
     private final char[] closeHook = {')'};                                     //32 --  1 << 5
     private final char[] openHook = {'('};                                      //64 --  1 << 6
@@ -21,12 +21,13 @@ public class Checker {
 
         int i = 0;
         int mode = 4+16+64;
-        return check(chars,i,mode, 0);
+        return check(chars,i,mode, 0,0);
     }
 
-    private boolean check(char[] chars, int i, int mode, int openHooks) {
+    private boolean check(char[] chars, int i, int mode, int openHooks, int delimeters) {
         if (mode==-1) return false;
         if (openHooks<0) return false;
+        if (delimeters>1) return false;
         if (i >=chars.length) {
             if (mode != 1+4+8+32 && mode != 1+32) return false;
             else return true;
@@ -35,12 +36,18 @@ public class Checker {
             boolean result = isAvailable(chars[i], mode);
             if (!result) return false;
             if (contains(closeHook,chars[i])) {
-                return check(chars, i+1, getCharMode(chars[i]), openHooks-1);
+                return check(chars, i+1, getCharMode(chars[i]), openHooks-1, 0);
             }
             if (contains(openHook, chars[i])) {
-                return check(chars, i+1, getCharMode(chars[i]), openHooks+1);
+                return check(chars, i+1, getCharMode(chars[i]), openHooks+1, 0);
             }
-            return check(chars, i+1, getCharMode(chars[i]), openHooks);
+            if (contains(delimiters, chars[i])) {
+                return check(chars, i+1, getCharMode(chars[i]), openHooks+1, delimeters+1);
+            }
+            if (contains(digits, chars[i])) {
+                return check(chars, i+1, getCharMode(chars[i]), openHooks+1, delimeters);
+            }
+            return check(chars, i+1, getCharMode(chars[i]), openHooks, 0);
         }
     }
 
@@ -96,7 +103,7 @@ public class Checker {
     public String removeBlanks(String s) {
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
-            if (Character.isDigit(c) || c=='.' || contains(hooks,c) || contains(mathOperations,c)) {
+            if (contains(digits,c) || contains(delimiters,c) || contains(hooks,c) || contains(mathOperations,c)) {
                 sb.append(c);
             }
         }
