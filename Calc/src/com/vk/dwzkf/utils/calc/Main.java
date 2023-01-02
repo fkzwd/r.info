dfa15f57a81f77045a7d5e51e2579ce16aee5ebd
Roman Shageev <shageev.r.f@gmail.com>
2023-01-02 20:42:36 +0300

    upd main method

--- Calc/src/com/vk/dwzkf/utils/calc/Main.java
+++ Calc/src/com/vk/dwzkf/utils/calc/Main.java
@@ -17,9 +17,10 @@ public class Main {
         for (int i = 0; i < 50; i++) {
             final int minLen = random.nextInt(60) + 3;
             System.out.print("Generate "+minLen+": ");
-            System.out.println(checker.generate(minLen));
+            String generated = checker.generate(minLen);
+            System.out.print(generated);
+            System.out.println(" Check: "+checker.check(generated));
         }
-        System.out.println(checker.check("0x-(1+5)+5-9+(5x/82,5^462)-(-8)*(-1x^2)+93/2*(-8x/5x/2)"));
 //        final int[] terminalModesArray = checker.findTerminalModesArray(checker.getCharMode(')'), 0);
 //        StringBuilder sb = new StringBuilder();
 //        checker.appendModes(sb, new Random(), terminalModesArray);
